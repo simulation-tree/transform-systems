7f9f45534441dde7a320bd56bd7470e086604a5b
Phill <philldamaskin@gmail.com>
2024-07-04 17:44:37 -0500

    initial commit

---
+++ source/Systems/TransformSystem.cs
@@ -0,0 +1,139 @@
+﻿using System.Numerics;
+using Transforms.Components;
+using Transforms.Events;
+using Unmanaged.Collections;
+
+namespace Game.Systems
+{
+    public class TransformSystem : SystemBase
+    {
+        private readonly Query<IsTransform> transformQuery;
+        private readonly Query<IsTransform, LocalToWorld> ltwQuery;
+        private readonly UnmanagedArray<EntityID> parentEntities;
+        private readonly UnmanagedArray<Matrix4x4> ltwValues;
+        private readonly UnmanagedList<UnmanagedList<uint>> sortedEntities;
+
+        public TransformSystem(World world) : base(world)
+        {
+            Subscribe<TransformUpdate>(Update);
+            transformQuery = new(world);
+            ltwQuery = new(world);
+            parentEntities = new();
+            ltwValues = new();
+            sortedEntities = new();
+        }
+
+        public override void Dispose()
+        {
+            foreach (UnmanagedList<uint> entities in sortedEntities)
+            {
+                entities.Dispose();
+            }
+
+            sortedEntities.Dispose();
+            ltwValues.Dispose();
+            parentEntities.Dispose();
+            ltwQuery.Dispose();
+            transformQuery.Dispose();
+            base.Dispose();
+        }
+
+        private void Update(TransformUpdate e)
+        {
+            //clear state
+            transformQuery.Fill();
+            parentEntities.Resize(transformQuery.Count + 1);
+            parentEntities.Clear();
+            ltwValues.Resize(transformQuery.Count + 1);
+            ltwValues.Clear();
+
+            //calculate ltp of all entities first
+            sortedEntities.Clear();
+            foreach (Query<IsTransform>.Result result in transformQuery)
+            {
+                EntityID parent = world.GetParent(result.entity);
+                parentEntities[result.entity.value] = parent;
+                ltwValues[result.entity.value] = CalculateLocalToParent(result.entity);
+
+                //calculate how deep the entity is
+                uint depth = 0;
+                EntityID current = parent;
+                while (current != default)
+                {
+                    depth++;
+                    current = world.GetParent(current);
+                }
+
+                if (sortedEntities.Count <= depth)
+                {
+                    sortedEntities.Add(new());
+                }
+
+                //put the entity into a list located at the index, where the index is the depth
+                ref UnmanagedList<uint> entities = ref sortedEntities.GetRef(depth);
+                entities.Add(result.entity.value);
+
+                //make sure it has an ltw component
+                if (!world.ContainsComponent<LocalToWorld>(result.entity))
+                {
+                    world.AddComponent<LocalToWorld>(result.entity, default);
+                }
+            }
+
+            //calculate ltw from ltp
+            foreach (UnmanagedList<uint> entities in sortedEntities)
+            {
+                foreach (uint entity in entities)
+                {
+                    EntityID parent = parentEntities[entity];
+                    ref Matrix4x4 ltp = ref ltwValues.GetRef(entity);
+                    if (parent != default)
+                    {
+                        ref Matrix4x4 parentLtp = ref ltwValues.GetRef(parent.value);
+                        ltp *= parentLtp;
+                    }
+                }
+
+                entities.Clear();
+            }
+
+            //apply ltw values
+            ltwQuery.Fill();
+            foreach (Query<IsTransform, LocalToWorld>.Result result in ltwQuery)
+            {
+                ref LocalToWorld ltw = ref result.Component2;
+                ltw.value = ltwValues[result.entity.value];
+            }
+        }
+
+        private Matrix4x4 CalculateLocalToParent(EntityID entity)
+        {
+            ref Position position = ref world.GetComponentRef<Position>(entity, out bool hasPosition);
+            ref EulerAngles eulerAngles = ref world.GetComponentRef<EulerAngles>(entity, out bool hasEulerAngles);
+            ref Rotation rotation = ref world.GetComponentRef<Rotation>(entity, out bool hasRotation);
+            ref Scale scale = ref world.GetComponentRef<Scale>(entity, out bool hasScale);
+            Matrix4x4 ltp = Matrix4x4.Identity;
+            if (hasScale)
+            {
+                ltp *= Matrix4x4.CreateScale(scale.value);
+            }
+
+            if (hasEulerAngles)
+            {
+                ltp *= Matrix4x4.CreateFromQuaternion(eulerAngles.AsQuaternion());
+            }
+
+            if (hasRotation)
+            {
+                ltp *= Matrix4x4.CreateFromQuaternion(rotation.value);
+            }
+
+            if (hasPosition)
+            {
+                ltp *= Matrix4x4.CreateTranslation(position.value);
+            }
+
+            return ltp;
+        }
+    }
+}
---
+++ tests/TransformTests.cs
@@ -0,0 +1,66 @@
+﻿using Game.Systems;
+using System.Numerics;
+using Transforms.Components;
+using Transforms.Events;
+using Unmanaged;
+
+namespace Game.Tests
+{
+    public class TransformTests
+    {
+        [TearDown]
+        public void CleanUp()
+        {
+            Allocations.ThrowIfAny();
+        }
+
+        [Test]
+        public void CreateLocalToWorldFromComponents()
+        {
+            using World world = new();
+            using TransformSystem transformSystem = new(world);
+
+            EntityID entity = world.CreateEntity();
+            world.AddComponent(entity, new Position(2, 2, 6));
+            world.AddComponent(entity, new Scale(2, 1, 2));
+            world.AddComponent(entity, new IsTransform());
+
+            world.Submit(new TransformUpdate());
+            world.Poll();
+
+            Assert.That(world.ContainsComponent<LocalToWorld>(entity), Is.True);
+            LocalToWorld ltw = world.GetComponent<LocalToWorld>(entity);
+            Assert.That(ltw.value, Is.EqualTo(Matrix4x4.CreateScale(2, 1, 2) * Matrix4x4.CreateTranslation(2, 2, 6)));
+        }
+
+        [Test]
+        public void ScaleChildRelativeToParent()
+        {
+            using World world = new();
+            using TransformSystem transformSystem = new(world);
+
+            EntityID parent = world.CreateEntity();
+            world.AddComponent(parent, new Position(5, 0, 0));
+            world.AddComponent(parent, EulerAngles.CreateFromDegrees(0f, 90f, 0f));
+            world.AddComponent(parent, new Scale(2, 2, 2));
+            world.AddComponent(parent, new IsTransform());
+
+            EntityID child = world.CreateEntity();
+            world.AddComponent(child, new Position(0, 0, 2));
+            world.AddComponent(child, new IsTransform());
+            world.SetParent(child, parent);
+
+            world.Submit(new TransformUpdate());
+            world.Poll();
+
+            Assert.That(world.ContainsComponent<LocalToWorld>(child), Is.True);
+            LocalToWorld ltw = world.GetComponent<LocalToWorld>(child);
+            Assert.That(ltw.Position.X, Is.InRange(9 - 0.02f, 9 + 0.02f));
+            Assert.That(ltw.Position.Y, Is.InRange(-0.02f, 0.02f));
+            Assert.That(ltw.Position.Z, Is.InRange(-0.02f, 0.02f));
+            Assert.That(ltw.Scale.X, Is.InRange(2 - 0.02f, 2 + 0.02f));
+            Assert.That(ltw.Scale.Y, Is.InRange(2 - 0.02f, 2 + 0.02f));
+            Assert.That(ltw.Scale.Z, Is.InRange(2 - 0.02f, 2 + 0.02f));
+        }
+    }
+}

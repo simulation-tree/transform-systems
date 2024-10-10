f56cfa00135ef2a85ee61aba9b38605b2477f746
Phill <philldamaskin@gmail.com>
2024-10-10 17:12:38 -0400

    Update TransformSystem.cs

--- source/Systems/TransformSystem.cs
+++ source/Systems/TransformSystem.cs
@@ -126,6 +126,7 @@ namespace Transforms.Systems
             }
 
             //calculate ltw in descending order (roots towards leafs)
+            //where each entity list is descending in depth
             anchorsQuery.Update(world);
             foreach (UnmanagedList<uint> entities in sortedEntities)
             {

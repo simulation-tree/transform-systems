ed04f6c6ea8b94d5cad60629ec16531c970e15de
Phill <philldamaskin@gmail.com>
2024-11-01 15:23:29 -0400

    update nunit package and changes after simulation

--- source/Systems/TransformSystem.cs
+++ source/Systems/TransformSystem.cs
@@ -23,7 +23,7 @@ namespace Transforms.Systems
         private readonly List<List<uint>> sortedEntities;
 
         readonly unsafe InitializeFunction ISystem.Initialize => new(&Initialize);
-        readonly unsafe IterateFunction ISystem.Update => new(&Update);
+        readonly unsafe IterateFunction ISystem.Iterate => new(&Update);
         readonly unsafe FinalizeFunction ISystem.Finalize => new(&Finalize);
 
         [UnmanagedCallersOnly]
